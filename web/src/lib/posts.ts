be502eaaefb309de1ba1fc529e0e1941786d45d1
Matt Hemsing <46225546+mrhemsing@users.noreply.github.com>
2026-02-21 13:49:10 -0800

    Hide post 14230905249 from archive output

--- web/src/lib/posts.ts
+++ web/src/lib/posts.ts
@@ -42,6 +42,7 @@ const HIDDEN_POST_IDS = new Set<string>([
   '90855799348',
   '70612824485',
   '65791216133',
+  '14230905249',
 ]);
 
 export async function getAllPosts(): Promise<TumblrPost[]> {
